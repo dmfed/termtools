005aaea69f5e70f2b32e5d0294770c21be7b79eb
Dmitry Fedotov <dafedotov@gmail.com>
2021-01-29 01:41:53 +0300

    changed sample program

--- sample/sample.go
+++ sample/sample.go
@@ -7,11 +7,19 @@ import (
 )
 
 func main() {
-	termtools.ClearScreen()                                            //clears screen
-	x, y := termtools.GetCurrTermSize()                                //gets size of terminal window
-	s := termtools.Reversed + termtools.Paint("blue", "Hello, world!") //adds blue color to string "Hello,world!"
-	termtools.PrintAtPositionAndReturn(x/2-len(s)/2, y/2, s)           //outputs string s in the middle of screen and returns cursor to initial position
-	s2 := termtools.Red + "We're gophers" + termtools.ColorReset
-	fmt.Println(s2)
-	fmt.Println(termtools.Paint("cyan", "We like Go!"))
+	// How to use Printer
+	var p termtools.Printer         // initializes Printer
+	p.SetColorID(9)                 // Sets color to orange
+	termtools.ClearScreen()         //clears screen
+	x, y := termtools.GetTermSize() //gets size of terminal window
+	s := p.Sprint("We are gophers")
+	// Below line outputs string s in the middle of screen and returns cursor to initial position
+	termtools.PrintAtPositionAndReturn(x/2-len(s)/2, y/2, s)
+	p.ToggleUnderline()
+	p.SetColor("green")
+	p.Println("We love Go")
+
+	// Simplest use is to call ColorSprint (same signature as in fmt.Sprint)
+	mystring := termtools.ColorSprint("red", "This will print in red")
+	fmt.Println(mystring)
 }
