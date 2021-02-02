4cd0ae3f252ccbb4f475714a5bcb51f78aeb0c17
Dmitry Fedotov <dafedotov@gmail.com>
2021-02-02 22:25:46 +0300

    refactored constants.go added map[string]string for bold, underline, reversed and blinking codes

--- constants.go
+++ constants.go
@@ -4,79 +4,81 @@ import "errors"
 
 const (
 	// The following constants hold ANSI escape sequences setting color and style of output
+	Esc string = "\x1b"
 
 	//Basic 8 colors
-	Black   string = "\u001b[30m"
-	Red            = "\u001b[31m"
-	Green          = "\u001b[32m"
-	Yellow         = "\u001b[33m"
-	Blue           = "\u001b[34m"
-	Magenta        = "\u001b[35m"
-	Cyan           = "\u001b[36m"
-	White          = "\u001b[37m"
+	Black   string = Esc + "[30m"
+	Red            = Esc + "[31m"
+	Green          = Esc + "[32m"
+	Yellow         = Esc + "[33m"
+	Blue           = Esc + "[34m"
+	Magenta        = Esc + "[35m"
+	Cyan           = Esc + "[36m"
+	White          = Esc + "[37m"
 
 	//Additional 8 bright colors
-	BrightBlack   string = "\u001b[30;1m"
-	BrightRed            = "\u001b[31;1m"
-	BrightGreen          = "\u001b[32;1m"
-	BrightYellow         = "\u001b[33;1m"
-	BrightBlue           = "\u001b[34;1m"
-	BrightMagenta        = "\u001b[35;1m"
-	BrightCyan           = "\u001b[36;1m"
-	BrightWhite          = "\u001b[37;1m"
+	BrightBlack   string = Esc + "[30;1m"
+	BrightRed            = Esc + "[31;1m"
+	BrightGreen          = Esc + "[32;1m"
+	BrightYellow         = Esc + "[33;1m"
+	BrightBlue           = Esc + "[34;1m"
+	BrightMagenta        = Esc + "[35;1m"
+	BrightCyan           = Esc + "[36;1m"
+	BrightWhite          = Esc + "[37;1m"
 
 	//Basic 8 background colors
-	BBlack   string = "\u001b[40m"
-	BRed            = "\u001b[41m"
-	BGreen          = "\u001b[42m"
-	BYellow         = "\u001b[43m"
-	BBlue           = "\u001b[44m"
-	BMagenta        = "\u001b[45m"
-	BCyan           = "\u001b[46m"
-	BWhite          = "\u001b[47m"
+	BBlack   string = Esc + "[40m"
+	BRed            = Esc + "[41m"
+	BGreen          = Esc + "[42m"
+	BYellow         = Esc + "[43m"
+	BBlue           = Esc + "[44m"
+	BMagenta        = Esc + "[45m"
+	BCyan           = Esc + "[46m"
+	BWhite          = Esc + "[47m"
 
 	//Additional 8 bright background colors
-	BBrightBlack   string = "\u001b[40;1m"
-	BBrightRed            = "\u001b[41;1m"
-	BBrightGreen          = "\u001b[42;1m"
-	BBrightYellow         = "\u001b[43;1m"
-	BBrightBlue           = "\u001b[44;1m"
-	BBrightMagenta        = "\u001b[45;1m"
-	BBrightCyan           = "\u001b[46;1m"
-	BBrightWhite          = "\u001b[47;1m"
+	BBrightBlack   string = Esc + "[40;1m"
+	BBrightRed            = Esc + "[41;1m"
+	BBrightGreen          = Esc + "[42;1m"
+	BBrightYellow         = Esc + "[43;1m"
+	BBrightBlue           = Esc + "[44;1m"
+	BBrightMagenta        = Esc + "[45;1m"
+	BBrightCyan           = Esc + "[46;1m"
+	BBrightWhite          = Esc + "[47;1m"
 
 	// Color format string to use with 256 color codes. Needs int in range [0;255].
-	ColorIDTemplate      string = "\u001b[38;5;%vm"
-	BackgroundIDTemplate        = "\u001b[48;5;%vm"
+	ColorIDTemplate      string = Esc + "[38;5;%vm"
+	BackgroundIDTemplate        = Esc + "[48;5;%vm"
 
 	//Styles. Can be used separately or together with color and background codes.
-	Bold      string = "\u001b[1m"
-	Underline        = "\u001b[4m"
-	Reversed         = "\u001b[7m"
+	Bold      string = Esc + "[1m"
+	Underline        = Esc + "[4m"
+	Blinking         = Esc + "[5m"
+	Reversed         = Esc + "[7m"
 
 	// Reset escape sequence
-	Reset string = "\u001b[0m"
+	Reset string = Esc + "[0m"
 
 	// Cursor maniputation
-	CursorSave                  string = "\033[s"
-	CursorRestore                      = "\033[u"
-	CursorGotoTemplate                 = "\033[%v;%vH"
-	CursorMoveUpTemplate               = "\u001b[%vA"
-	CursorMoveDownTemplate             = "\u001b[%vB"
-	CursorMoveRightTemplate            = "\u001b[%vC"
-	CursorMoveLeftTemplate             = "\u001b[%vD"
-	CursorMoveToNextRowTemplate        = "\u001b[E"
-	CursorMoveToRowTemplate            = "\u001b[%vH"
+	CursorSave                  string = Esc + "[s"
+	CursorRestore                      = Esc + "[u"
+	CursorGotoTemplate                 = Esc + "[%v;%vH"
+	CursorMoveUpTemplate               = Esc + "[%vA"
+	CursorMoveDownTemplate             = Esc + "[%vB"
+	CursorMoveRightTemplate            = Esc + "[%vC"
+	CursorMoveLeftTemplate             = Esc + "[%vD"
+	CursorMoveToNextRowTemplate        = Esc + "[E"
+	CursorMoveToRowTemplate            = Esc + "[%vH"
 
 	// Clear screen codes
-	Clear     string = "\u001b[2J"
-	ClearUp          = "\u001b[1J"
-	ClearDown        = "\u001b[0J"
+	Clear     string = Esc + "[2J"
+	ClearUp          = Esc + "[1J"
+	ClearDown        = Esc + "[0J"
 
 	// Clear line codes
-	ClearL      string = "\u001b[2K"
-	ClearLLeft         = "\u001b[1K"
-	ClearLRight        = "\u001b[0K"
+	ClearL      string = Esc + "[2K"
+	ClearLLeft         = Esc + "[1K"
+	ClearLRight        = Esc + "[0K"
 )
 
 var (
@@ -119,4 +121,9 @@ var (
 		"brightmagenta": BBrightMagenta,
 		"brightcyan":    BBrightCyan,
 		"brightwhite":   BBrightWhite}
+	modeMap = map[string]string{
+		"bold":      Bold,
+		"underline": Underline,
+		"blinking":  Blinking,
+		"reversed":  Reversed}
 )
